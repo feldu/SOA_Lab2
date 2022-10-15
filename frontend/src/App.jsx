3a41222da75232c66bbef732485083501c7aeb95
Egor Ostryakov <eg.ostr@mail.ru>
2022-10-16 02:32:14 +0300

    add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import {Button} from "@chakra-ui/react";
+
+function App() {
+    return (
+        <div className="App">
+            <Button colorScheme='teal'>Hello sukka</Button>
+        </div>
+    );
+}
+
+export default App;
