5560c499d2cb89e8b8d3cb8a0c9605dae8b1f2fa
Egor Ostryakov <eg.ostr@mail.ru>
2022-10-16 16:40:30 +0300

    ah shit, here we go again...

---
+++ frontend/src/components/NumberControl.jsx
@@ -0,0 +1,24 @@
+import {
+    FormControl,
+    FormLabel,
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper
+} from "@chakra-ui/react";
+
+export default function NumberControl({label, value, setValue, min}) {
+    return (
+        <FormControl direction="column" w="full" justifyContent="center" alignItems="center" flex={1}>
+            <FormLabel>{label}</FormLabel>
+            <NumberInput defaultValue={0} min={min} value={value} onChane={e => setValue(e.target.value)}>
+                <NumberInputField/>
+                <NumberInputStepper>
+                    <NumberIncrementStepper/>
+                    <NumberDecrementStepper/>
+                </NumberInputStepper>
+            </NumberInput>
+        </FormControl>
+    );
+}
---
+++ frontend/src/pages/UserPage/UserPage.jsx
@@ -0,0 +1,24 @@
+import {Flex} from "@chakra-ui/react";
+import MainHeader from "../../components/MainHeader";
+import GetOrgsForm from "./components/GetOrgsForm";
+
+export default function UserPage() {
+    return (
+        <Flex direction="column">
+            <MainHeader/>
+            <Flex direction="column" w="full" justifyContent="center" alignItems="center" flex={1}
+                  p={5} mx="auto">
+                <Flex direction="row" w="100%" alignItems="stretch" mt={5}>
+                    <GetOrgsForm/>
+                    <GetOrgsForm/>
+                    <GetOrgsForm/>
+                </Flex>
+                <Flex direction="row" w="100%" alignItems="stretch" mt={5}>
+                    <GetOrgsForm/>
+                    <GetOrgsForm/>
+                    <GetOrgsForm/>
+                </Flex>
+            </Flex>
+        </Flex>
+    );
+}
