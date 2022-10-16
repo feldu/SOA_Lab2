6cb5df06b5897990e7eaf05d46221aad3980b96b
Egor Ostryakov <eg.ostr@mail.ru>
2022-10-17 01:13:07 +0300

    add main controller and log

--- MainService/src/main/java/soa/lab/controller/CrudController.java
+++ MainService/src/main/java/soa/lab/controller/CrudController.java
@@ -24,28 +24,37 @@ public class CrudController {
 
     @GetMapping("/orgs")
     public ResponseEntity<List<Organization>> getOrgs(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer size, @RequestParam(required = false) String filter, @RequestParam(required = false) String sort) {
+        log.info("Request to get orgs with params: \npage: {}, \nsize: {}, \nfilter: {}, \nsort: {}", page, size, filter, sort);
+        List<Organization> orgs = organizationService.getOrgs(page, size, filter, sort);
+        log.info("GET request handled. Returning collection with {} elements", orgs.size());
         return new ResponseEntity<>(organizationService.getOrgs(page, size, filter, sort), HttpStatus.OK);
     }
 
     @PostMapping("/orgs")
     public ResponseEntity<Organization> addOrg(@RequestBody OrganizationDTO orgDTO) {
+        log.info("Request to add org with body: {}", orgDTO);
         Organization org = new Organization(orgDTO);
+        log.info("Request to add org handled");
         return new ResponseEntity<>(organizationService.addOrg(org), HttpStatus.OK);
     }
 
     @PutMapping("/orgs")
     public ResponseEntity<Organization> updateOrg(@RequestBody OrganizationDTO orgDTO) {
+        log.info("Request to update org with body: {}", orgDTO);
         Organization orgUpd = new Organization(orgDTO);
+        log.info("Request to update org handled");
         return new ResponseEntity<>(organizationService.updateOrg(orgDTO.getId(), orgUpd), HttpStatus.OK);
     }
 
     @GetMapping("/orgs/{id}")
     public ResponseEntity<Organization> getOrgById(@PathVariable Long id) {
+        log.info("Request to get org with id: {}", id);
         return new ResponseEntity<>(organizationService.getOrgById(id), HttpStatus.OK);
     }
 
     @DeleteMapping("/orgs/{id}")
     public ResponseEntity<Organization> deleteOrgById(@PathVariable Long id) {
+        log.info("Request to delete org with id: {}", id);
         return new ResponseEntity<>(organizationService.deleteOrgById(id), HttpStatus.OK);
     }
 }
