88de11618c5991e4ebfe787db37482f8e12fbb69
Egor Ostryakov <eg.ostr@mail.ru>
2022-10-29 16:00:49 +0300

    add different client error messages for 4xx

--- frontend/src/globalState/orgs/index.js
+++ frontend/src/globalState/orgs/index.js
@@ -6,6 +6,23 @@ export const orgsState = observable({
         orgs: observable([]),
         response: {isError: null, message: ""},
 
+        handleError: function (e) {
+            this.orgs = [];
+            if (e.response.status === 400) {
+                this.response = {isError: true, message: "Bad request"};
+                if (e.response.data.message.includes("parser")) this.response.message = "Filter string parsing error";
+                if (e.response.data.message.includes("No property")) this.response.message = e.response.data.message;
+                if (e.response.data.message.includes("IllegalStateException")) this.response.message = "Invalid arguments error";
+                if (e.response.data.message.includes("ManagedType")) this.response.message = "Invalid arguments error";
+                if (e.response.data.message.includes("IllegalStateException")) this.response.message = "Invalid arguments error";
+                if (e.response.data.message.includes("out of bounds")) this.response.message = "Invalid arguments error";
+            }
+            if (e.response.status === 404)
+                this.response = {isError: true, title: "Not found", message: e.response.data.message};
+            if (e.response.status === 500)
+                this.response = {isError: true, message: "Server error"};
+        },
+
         fetchOrgs(params) {
             axios
                 .get("https://localhost:31510/orgs", {params})
@@ -13,32 +30,21 @@ export const orgsState = observable({
                     this.orgs = response.data;
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
 
         addOrgs(params) {
             axios
-                .post("https://localhost:31510/orgs", params)
+                .post("https://localhost:31510/orgs", {...params, type: params.type === "" ? null : params.type})
                 .then(response => {
                     this.orgs = [response.data];
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
+
         updateOrgs(params) {
             axios
                 .put("https://localhost:31510/orgs", params)
@@ -46,15 +52,10 @@ export const orgsState = observable({
                     this.orgs = [response.data];
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
+
         getOrgById(pathVariable) {
             axios
                 .get(`https://localhost:31510/orgs/${pathVariable}`)
@@ -62,15 +63,10 @@ export const orgsState = observable({
                     this.orgs = [response.data];
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
+
         deleteOrgById(pathVariable) {
             axios
                 .delete(`https://localhost:31510/orgs/${pathVariable}`)
@@ -78,13 +74,7 @@ export const orgsState = observable({
                     this.orgs = [response.data];
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
 
@@ -95,15 +85,10 @@ export const orgsState = observable({
                     this.orgs = response.data;
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
+
         getFilteredOrgsByAnal(pathVar1, pathVar2) {
             axios
                 .get(`https://localhost:31511/orgdirectory/filter/turnover/${pathVar1}/${pathVar2}`)
@@ -111,15 +96,10 @@ export const orgsState = observable({
                     this.orgs = response.data;
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
+
         getFilteredOrgsByEmployees(pathVar1, pathVar2) {
             axios
                 .get(`https://localhost:31511/orgdirectory/filter/employees/${pathVar1}/${pathVar2}`)
@@ -127,13 +107,7 @@ export const orgsState = observable({
                     this.orgs = response.data;
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
 
@@ -141,17 +115,10 @@ export const orgsState = observable({
             axios
                 .get("https://localhost:31510/orgs/group-count/by-address")
                 .then(response => {
-                    console.log(response.data)
                     this.orgs = []
                     this.response = {isError: false, message: `Groups: ${response.data}`}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
 
@@ -162,13 +129,7 @@ export const orgsState = observable({
                     this.orgs = response.data;
                     this.response = {isError: false, message: "Request successful"}
                 }).catch(e => {
-                this.orgs = [];
-                if (e.response.status === 400)
-                    this.response = {isError: true, message: "Bad request"};
-                if (e.response.status === 404)
-                    this.response = {isError: true, message: "Not found"};
-                if (e.response.status === 500)
-                    this.response = {isError: true, message: "Server error"};
+                this.handleError(e);
             });
         },
 
